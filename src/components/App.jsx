183791bd789112f4be67c46e95621e615b77a051
jaidernunes <jaidernf@gmail.com>
2022-10-07 16:06:25 -0300

    test commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+// import {
+//   BrowserRouter,
+//   // Switch,
+//   Route,
+//   // Link,
+// } from 'react-router-dom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="mainApp">
+
+      </div>
+      // <BrowserRouter>
+      //   <Route path="/" component={ App } />
+      // </BrowserRouter>
+    );
+  }
+}
+
+// export default App;
