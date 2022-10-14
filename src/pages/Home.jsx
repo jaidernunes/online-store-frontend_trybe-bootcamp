02a1de3a44cef9b11c129d5c083599ae801f91ea
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-10-14 19:01:22 -0300

    corrigindo conflitos em main-group-2-bananasdpj-release antes de fazer o merge com a branch main-group-2-bananasdpj-req-8

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes, { array } from 'prop-types';
 import { Link } from 'react-router-dom';
 import Categories from '../components/Categories';
-import { getProductsFromCategoryAndQuery, getCategories } from '../services/api';
+import { getProductsFromCategoryAndQuery, getCategories, getProductById } from '../services/api';
+import saveCartItems from '../helpers/saveCartItems';
+// const listaNoCarrinho = document.querySelector('ol.cart__items'); // tag do tipo <ol> // usei querySelecctor pois facilita trabalhar com forEach
 
 const homeInitialMessage = (
   <h3 data-testid="home-initial-message">
@@ -10,15 +13,24 @@ const homeInitialMessage = (
 );
 const searchNoneFound = (<h3>Nenhum produto foi encontrado</h3>);
 
-export default class Home extends React.Component {
-  state = {
-    homeDisplay: homeInitialMessage,
-    searchInput: '',
-    categoriesList: [],
-  };
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      homeDisplay: homeInitialMessage,
+      searchInput: '',
+      categoriesList: [],
+      array: [],
+    };
+  }
 
   componentDidMount() {
     this.getListCategories();
+    // this.callCreateCartItemElement();
+  }
+
+  componentDidUpdate() {
+    this.callCreateCartItemElement();
   }
 
   getListCategories = async () => {
@@ -27,49 +39,107 @@ export default class Home extends React.Component {
     this.setState({ categoriesList });
   };
 
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value; // ramiro: haverá um checkbox?
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  // ramiro: inicio funções req 8
+  createCartItemElement = ({ id, title, price }) => {
+    const li = document.createElement('li');
+    li.className = 'cart__item';
+    li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
+    // li.addEventListener('click', cartRemoveItem(li));
+    console.log(li);
+    return li;
+  };
+
+  callCreateCartItemElement = () => {
+    const botoesAddCart = document.querySelectorAll('.item__add');
+    // console.log(botoesAddCart);
+    // console.log(this.props);
+    // vou pegar todos os botoes add ao carrinho e mapea-los com forEach
+    botoesAddCart.forEach((botao) => {
+      botao.addEventListener('click', async () => {
+        // console.log(botao);
+        const idDoProduto = botao.id;
+        console.log(idDoProduto);
+        console.log('entrei');
+        const data = await getProductById(idDoProduto);// getProductById tem a mesma utilidade de fetchItem de shoppingCart project
+        // console.log(data);
+
+        const li = document.createElement('li');
+        li.className = 'cart__item';
+        li.innerText = `ID: ${data.id} | TITLE: ${data.title} | PRICE: $${data.price}`;
+        // li.addEventListener('click', cartRemoveItem(li));
+        // console.log(li.innerHTML);
+        const arrayAux = [];
+
+        arrayAux.push(li);
+        // console.log(arrayAux);
+        this.setState((prevState) => ({
+          array: [...prevState.array, arrayAux],
+        }), () => console.log(this.state.array));
+        // // alert(fetchItemProduto);
+        // listaNoCarrinho.innerHTML(li);
+        // console.log(array);
+        // saveCartItems(arrayAux);
+      });
+    });
+  }; // ramiro: fim funções req 8
+
+  handleSearch = async (query) => {
+    const searchResponse = await getProductsFromCategoryAndQuery(undefined, query);
+    // console.log(searchResponse);
+    const searchResults = searchResponse.results;
+
+    // RETORNA MENSAGEM SE NAO ENCONTRAR NADA
+    if (searchResults.length === 0) {
+      this.setState({
+        homeDisplay: searchNoneFound,
+      });
+      return;
+    }
+
+    // MOSTRA TODOS OS ENCONTRADOS
+    const resultsMap = await searchResults.map((item) => (
+      <div key={ item.id } data-testid="product">
+        <h4>{item.title}</h4>
+        <img src={ item.thumbnail } alt={ item.title } />
+        <h5>{item.price}</h5>
+        <Link
+          to={ `details/${item.id}` }
+          data-testid="product-detail-link"
+        >
+          Detalhes
+        </Link>
+        {/* ramiro: botao req 8 */}
+        <button
+          data-testid="product-add-to-cart"
+          type="button"
+          label="cartButton"
+          name="cartButton"
+          className="item__add"
+          id={ `${item.id}` }
+        //   onClick={ () => this.callCreateCartItemElement() }
+        >
+          Adicionar ao carrinho
+        </button>
+      </div>
+    ));
+    //   callCreateCartItemElement();
+    this.setState({
+      homeDisplay: resultsMap,
+    });
+  };
+
   render() {
     const { state } = this;
     const { searchInput, categoriesList } = state;
-
-    const handleChange = ({ target }) => {
-      const { name } = target;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-
-      this.setState({
-        [name]: value,
-      });
-    };
-
-    const handleSearch = async (query) => {
-      const searchResponse = await getProductsFromCategoryAndQuery(undefined, query);
-      const searchResults = searchResponse.results;
-
-      // RETORNA MENSAGEM SE NAO ENCONTRAR NADA
-      if (searchResults.length === 0) {
-        this.setState({
-          homeDisplay: searchNoneFound,
-        });
-        return;
-      }
-
-      // MOSTRA TODOS OS ENCONTRADOS
-      const resultsMap = await searchResults.map((item) => (
-        <div key={ item.id } data-testid="product">
-          <h4>{item.title}</h4>
-          <img src={ item.thumbnail } alt={ item.title } />
-          <h5>{item.price}</h5>
-          <Link
-            to={ `details/${item.id}` }
-            data-testid="product-detail-link"
-          >
-            Detalhes
-          </Link>
-        </div>
-      ));
-      this.setState({
-        homeDisplay: resultsMap,
-      });
-    };
 
     return (
       <>
@@ -95,7 +165,7 @@ export default class Home extends React.Component {
             name="searchInput"
             id="searchInput"
             value={ searchInput }
-            onChange={ handleChange }
+            onChange={ this.handleChange }
           />
           <button
             type="button"
@@ -113,12 +183,14 @@ export default class Home extends React.Component {
             data-testid="shopping-cart-button"
             to="/cart"
           >
-            Enter
+            Carrinho
+            {/* ramiro: adicionei carrinho para melhor especificar e destacar o link */}
           </Link>
         </div>
 
         <div className="displaySearch">
           { state.homeDisplay }
+          {/* {this.callCreateCartItemElement()} */}
           { // COMENTEI NO MERGE - JAIDER
             // <input type="text"/>
           }
@@ -128,3 +200,12 @@ export default class Home extends React.Component {
     );
   }
 }
+
+Home.propTypes = {
+  handleChange: PropTypes.func,
+  handleSearch: PropTypes.func,
+  createCartItemElement: PropTypes.func,
+  callCreateCartItemElement: PropTypes.func,
+}.isRequired;
+
+export default Home;
